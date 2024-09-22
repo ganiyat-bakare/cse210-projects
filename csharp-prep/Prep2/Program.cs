e9b9612881894f2b4041c383f274bbfae59ef912
ganiyat-bakare <bak24006@byui.edu>
2024-09-22 23:53:24 +0100

    completed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+
+        }
     }
 }
