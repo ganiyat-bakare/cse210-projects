2e86d03298e349973bf554bc72288cac1b059d6a
ganiyat-bakare <bak24006@byui.edu>
2024-09-23 02:14:16 +0100

    prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    static void Main(string[] args) 
+    {     
+        int guess = -1;
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Guess the magic number? ");
+            string answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+       } 
     }
 }
+    
