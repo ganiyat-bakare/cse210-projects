efebf677560681600cfe05187187c4fb64db2cc8
ganiyat-bakare <167991298+ganiyat-bakare@users.noreply.github.com>
2024-10-23 21:07:12 +0100

    learning06 added

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Red", 2);
+        shapes.Add(shape1);
+        
+        Rectangle shape2 = new Rectangle("Pink", 4, 6);
+        shapes.Add(shape2);
+        
+        Circle shape3 = new Circle("Blue", 8);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+        
     }
 }
